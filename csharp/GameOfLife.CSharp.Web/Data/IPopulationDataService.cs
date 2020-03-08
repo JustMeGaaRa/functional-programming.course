d4f91e3aa849edb57535e3716dfb83c8da7782e9
Vitalii Skovron <skovron.vitalii@gmail.com>
2020-03-08 23:04:06 +0200

    improvement: changed html content from static to component based

---
+++ csharp/GameOfLife.CSharp.Web/Data/IPopulationDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameOfLife.CSharp.Web.Data
+{
+    public interface IPopulationDataService
+    {
+        Task<IList<List<string>>> GetPopulationData();
+    }
+}
