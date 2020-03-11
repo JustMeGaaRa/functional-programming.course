4fb9368489b0294c819f799e57e067ae7270bf24
Vitalii Skovron <skovron.vitalii@gmail.com>
2020-03-11 23:06:56 +0200

    improvement: moved code from ui components to components code behind

---
+++ csharp/GameOfLife.CSharp.Web/Components/Grid.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace GameOfLife.CSharp.Web.Components
+{
+    public class GridBase: ComponentBase
+    {
+        [Parameter]
+        public IList<List<string>> Data { get; set; }
+
+        protected override void OnInitialized() => Data = Data;
+    }
+}
---
+++ csharp/GameOfLife.CSharp.Web/Components/Row.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace GameOfLife.CSharp.Web.Components
+{
+    public class RowBase : ComponentBase
+    {
+        [Parameter]
+        public IList<string> Data { get; set; }
+
+        protected override void OnInitialized() => Data = Data;
+    }
+}
