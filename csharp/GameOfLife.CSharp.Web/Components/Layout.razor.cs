e1efa4595c1999caca47a3ee8693731950439ea2
Vitalii Skovron <skovron.vitalii@gmail.com>
2020-03-11 23:06:56 +0200

    improvement: moved code from ui components to components code behind

---
+++ csharp/GameOfLife.CSharp.Web/Components/Layout.razor.cs
@@ -0,0 +1,19 @@
+﻿using GameOfLife.CSharp.Web.Data;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameOfLife.CSharp.Web.Components
+{
+    public class LayoutBase : ComponentBase
+    {
+        [Inject] IPopulationDataService PopulationDataService { get; set; }
+
+        protected IList<List<string>> _populationData;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _populationData = await PopulationDataService.GetPopulationData();
+        }
+    }
+}
