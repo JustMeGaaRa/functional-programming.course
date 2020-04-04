2253b3d06393813c0006bfd84ad2c62240552f02
Vitalii Skovron <skovron.vitalii@gmail.com>
2020-04-04 14:03:24 +0300

    improvement: added patterns service, component; changed patterns ui from static to dynamic one

---
+++ csharp/GameOfLife.CSharp.Web/Data/PatternsService.cs
@@ -0,0 +1,32 @@
+﻿using GameOfLife.CSharp.Web.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GameOfLife.CSharp.Web.Data
+{
+    public class PatternsService : IPatternsService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PatternsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ICollection<Pattern>> GetPattersByUserId(int userId)
+        {
+            var url = $"api/users/{userId}/patterns";
+
+            _httpClient.BaseAddress = new Uri("https://localhost:44370");
+
+            var response = await _httpClient.GetJsonAsync<ICollection<Pattern>>(url);
+
+            return response;
+        }
+    }
+}
---
+++ csharp/GameOfLife.CSharp.Web/Models/Pattern.cs
@@ -0,0 +1,10 @@
+﻿namespace GameOfLife.CSharp.Web.Models
+{
+    public class Pattern
+    {
+        public int PatternId { get; set; }
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height {get;set;}
+    }
+}
